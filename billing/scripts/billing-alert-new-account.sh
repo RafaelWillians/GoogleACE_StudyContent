70fb96613a3e565573152b96fe27da2a46166c35
Rafael Silva Willians <rafael.willians@outlook.com>
2024-09-18 19:48:11 +0000

    Adicionada info no script

---
+++ billing/scripts/billing-alert-new-account.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Script para criar alerta de orçamento,
+# quando o gasto ultrapassar determinado valor passado por parâmetro
+# pelo usuário 
+
+# Por padrão este script irá checar a billing account (conta de faturamento)
+# padrão e utilizá-la para criar o alerta
+
+# Checa a billing account padrão criada e atribui à variável BILLING_ACCOUNT
+
+BILLING_ACCOUNT=$(gcloud billing accounts list --format='value(name)')
+
+
+
+
+
+
+
+
