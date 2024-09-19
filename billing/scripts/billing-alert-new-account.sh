cb8f60afea076c9195e38323537466401478790c
Rafael Silva Willians <rafael.willians@outlook.com>
2024-09-19 18:40:17 +0000

    Corrigidos outputs

--- billing/scripts/billing-alert-new-account.sh
+++ billing/scripts/billing-alert-new-account.sh
@@ -19,11 +19,11 @@ read -p "Insert one budget ammount in default currency(ex. 0.50 or 123.75): " \
 
 # Faz a leitura do percentual de limite (threshold)
 
-read -p "Insert one threshold value percent(ex. '0.50' for a 50% threshold)" \
+read -p "Insert one threshold value percent(ex. '0.50' for a 50% threshold): " \
   THRESHOLD_PERCENT
 
 # Faz a leitura do tipo de base de cálculo (basis), se atualmente gasta ou prevista
-read -p "Insert 'c' for current-spend basis or 'p' for forecasted-spend basis" \
+read -p "Insert 'c' for current-spend basis or 'p' for forecasted-spend basis: " \
   THRESHOLD_BASIS
 
 # Faz a leitura do email a ser adicionado para alerta
