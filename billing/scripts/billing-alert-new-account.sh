004d400242b7de91c78e616103952ef1954fa13a
Rafael Silva Willians <rafael.willians@outlook.com>
2024-09-19 14:30:16 +0000

    Adicionado conteudo no script

--- billing/scripts/billing-alert-new-account.sh
+++ billing/scripts/billing-alert-new-account.sh
@@ -8,9 +8,20 @@
 # padrão e utilizá-la para criar o alerta
 
 # Checa a billing account padrão criada e atribui à variável BILLING_ACCOUNT
-
 BILLING_ACCOUNT=$(gcloud billing accounts list --format='value(name)')
 
+# Atribui à variável os limites(threshold) que o usuário inserir como parâmetro
+BUDGET_THRESHOLD=$1
+
+# Verifica se foram passados os limites como argumento. 
+if [ -z "$1" ]; then
+  echo "Budget threshold not provided."
+  exit 1
+fi
+
+
+
+gcloud
 
 
 
