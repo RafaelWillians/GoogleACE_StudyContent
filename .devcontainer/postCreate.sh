fffe23e639b3c3770fe09663c56799da9bc2894c
Rafael Silva Willians <rafael.willians@outlook.com>
2024-09-23 16:57:10 +0000

    Alterado devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "init"
+
 # Authenticate to gcloud
 gcloud auth login
 
 # Set the default project for the user
 gcloud config set project $(gcloud projects list --format='value(projectId)' --limit=1)
+
+$SHELL
